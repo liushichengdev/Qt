205f963b8bb69ed24265b3f6864ee4c143736822
liushicheng <liushichengdev@163.com>
2022-06-20 14:50:02 +0800

    First Qt GUI Application

---
+++ FirstQtGUIApp/mainwindow.cpp
@@ -0,0 +1,24 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDebug>
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    qDebug()<<"User clicked on a button";
+    QMessageBox::information(this,"Message", "You Clicked on that button",QMessageBox::Ok);
+}
+
