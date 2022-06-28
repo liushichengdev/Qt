ce5078d5ec6aa64d4edaf5207483f995f20aa7b9
liushicheng <liushichengdev@163.com>
2022-06-28 19:13:10 +0800

    Add Sharing Resources between threads using QMutex and QMutexLocker

---
+++ SharingResouces/worker.cpp
@@ -0,0 +1,38 @@
+#include "worker.h"
+
+Worker::Worker(QObject *parent)
+    : QObject{parent}
+{
+
+}
+
+void Worker::run()
+{
+    //Threadpool runs this code
+
+    for(int i=0;i<10;i++){
+
+        //QMutexLocker locker(m_mutex);// Lock - pay attention to Scope
+        m_mutex->lock();
+        int value=*m_count;
+        value++;
+        *m_count=value; //DANGER!!!!
+
+        qInfo()<<QThread::currentThread()<<*m_count;
+
+        //unlock
+        m_mutex->unlock();
+    }
+
+    qInfo()<<QThread::currentThread()<<"Done";
+}
+
+void Worker::setCount(int *newCount)
+{
+    m_count = newCount;
+}
+
+void Worker::setMutex(QMutex *newMutex)
+{
+    m_mutex = newMutex;
+}
