48ac5ec805eda05114b92a8c200d6985bae42674
liushicheng <liushichengdev@163.com>
2022-07-16 13:23:38 +0800

    Add QFileSystemModel

---
+++ QFileSystemModel/dialog.cpp
@@ -0,0 +1,35 @@
+#include "dialog.h"
+#include "./ui_dialog.h"
+
+Dialog::Dialog(QWidget *parent)
+    : QDialog(parent)
+    , ui(new Ui::Dialog)
+{
+    ui->setupUi(this);
+
+    QString sPath="C:/";
+    dirModel=new QFileSystemModel(this);
+    dirModel->setRootPath(sPath);
+    dirModel->setFilter(QDir::NoDotAndDotDot |QDir::AllDirs);
+    ui->treeView->setModel(dirModel);
+
+    fileModel=new QFileSystemModel(this);
+    fileModel->setRootPath(sPath);
+    fileModel->setFilter(QDir::NoDotAndDotDot |QDir::Files);
+
+    ui->listView->setModel(fileModel);
+}
+
+Dialog::~Dialog()
+{
+    delete ui;
+}
+
+
+void Dialog::on_treeView_clicked(const QModelIndex &index)
+{
+    QString sPath=dirModel->fileInfo(index).absoluteFilePath();
+
+    ui->listView->setRootIndex(fileModel->setRootPath(sPath));
+}
+
