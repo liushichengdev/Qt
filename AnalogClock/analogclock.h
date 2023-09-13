5705557f6130db7cd34ff3833641589a6b58a37c
liushicheng <liushichengdev@163.com>
2023-09-13 14:24:31 +0800

    New Project Analog Clock

---
+++ AnalogClock/analogclock.h
@@ -0,0 +1,23 @@
+#ifndef ANALOGCLOCK_H
+#define ANALOGCLOCK_H
+
+#include <QWidget>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class AnalogClock; }
+QT_END_NAMESPACE
+
+class AnalogClock : public QWidget
+{
+    Q_OBJECT
+
+public:
+    AnalogClock(QWidget *parent = nullptr);
+    ~AnalogClock();
+
+private:
+    Ui::AnalogClock *ui;
+
+    void paintEvent(QPaintEvent *event) override;
+};
+#endif // ANALOGCLOCK_H
